8490baba0ee53d8608bf8cb9a1d8b3fa12ec7c53
Mathias Helsen <mathias@raspbpi.(none)>
2014-01-09 14:38:05 +0000

    added controller software

---
+++ Controller/main.c
@@ -0,0 +1,100 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdint.h>
+
+#include <wand/MagickWand.h>
+
+#include "mydefs.h"
+
+int
+set_interface_attribs (int fd, int speed, int parity)
+{
+        struct termios tty;
+        memset (&tty, 0, sizeof tty);
+        if (tcgetattr (fd, &tty) != 0)
+        {
+                printf("error %d from tcgetattr", errno);
+                return -1;
+        }
+
+        cfsetospeed (&tty, speed);
+        cfsetispeed (&tty, speed);
+
+        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+        // disable IGNBRK for mismatched speed tests; otherwise receive break
+        // as \000 chars
+        tty.c_iflag &= ~IGNBRK;         // ignore break signal
+        tty.c_lflag = 0;                // no signaling chars, no echo,
+                                        // no canonical processing
+        tty.c_oflag = 0;                // no remapping, no delays
+        tty.c_cc[VMIN]  = 0;            // read doesn't block
+        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+
+        tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
+                                        // enable reading
+        tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
+        tty.c_cflag |= parity;
+        tty.c_cflag &= ~CSTOPB;
+        tty.c_cflag &= ~CRTSCTS;
+
+        if (tcsetattr (fd, TCSANOW, &tty) != 0)
+        {
+                printf("error %d from tcsetattr", errno);
+                return -1;
+        }
+        return 0;
+}
+
+void
+set_blocking (int fd, int should_block)
+{
+        struct termios tty;
+        memset (&tty, 0, sizeof tty);
+        if (tcgetattr (fd, &tty) != 0)
+        {
+                printf("error %d from tggetattr", errno);
+                return;
+        }
+
+        tty.c_cc[VMIN]  = should_block ? 1 : 0;
+        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+        if (tcsetattr (fd, TCSANOW, &tty) != 0)
+                printf("error %d setting term attributes", errno);
+}
+
+int main(int argc, char **argv)
+{
+	char *portname = "/dev/ttyAMA0";
+	int fd = open( portname, O_RDWR | O_NOCTTY | O_SYNC );
+	if (fd < 0)
+	{
+        	printf("error %d opening %s: %s", errno, portname, strerror (errno));
+	        return EXIT_FAILURE;
+	}
+
+	set_interface_attribs (fd, B115200, 0);  // set speed to 115,200 bps, 8n1 (no parity)
+	set_blocking (fd, 0);                // set no blocking
+
+	char out[4];
+	uint16_t value = 0xaabb;
+	out[0] = IN_CMD_SET_PIXELS;
+	out[1] = 0x02;
+	out[2] = (char)(value & 0x00ff);
+	out[3] = (char)(value >> 8);
+	printf("Everything is ready. Press ENTER to continue\n");
+	getchar();
+	printf("writing: 0x%X 0x%X 0x%X 0x%X\n", out[0], out[1], out[2], out[3]);
+	int ret = write (fd, out, 4);
+	printf("write returned: %d\n", ret);
+	
+	return EXIT_SUCCESS;
+}
