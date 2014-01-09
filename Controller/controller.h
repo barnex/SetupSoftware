a2eb99efd47ca611c202e16d167f31e1cc692443
Mathias Helsen <mathias@mona.ugent.be>
2014-01-09 16:16:40 +0100

    adding software

--- Controller/controller.h
+++ Controller/controller.h
@@ -1,4 +1,19 @@
 #ifndef _CONTROLLER_H
 #define _CONTROLLER_H
 
+int abort();
+
+int setStart(int *start);
+int setIInc(int *i_inc);
+int setJInc(int *j_inc);
+int setTSettle(int t_settle);
+int setPixels(int pixels);
+
+int gotoPosition(int *position);
+int getPosition(int *position);
+int getChannels(int *values);
+
+int 1DScan(uint16_t *pixelList, int wait_time);
+int 2DScan(uint16_t *pixelList, const char *filename);
+
 #endif
