e83ff182f82c5d035acce47c5421ea83643afd09
Mathias Helsen <Mathias.Helsen@UGent.be>
2014-03-14 16:12:55 +0100

    e

--- Example/main.c
+++ Example/main.c
@@ -4,25 +4,25 @@
 #include <signal.h>
 #include <math.h>
 #include <assert.h>
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
     int sockfd = 0;
 
-    initClient( &sockfd, 5001);
+    initClient( &sockfd, 5002);
 
-    int32_t returnValue[2] = {0, 0};
-    float   measurement[8];
-	myWrite( sockfd, "MEAS,0.1\n" );
-	myReadfull( sockfd, (void *)returnValue, sizeof(int32_t)*2 );
-	int i = 0;
-	float resultfft[3] = {0.0, 0.0, 0.0};
-	while( i < returnValue[1]/(sizeof(float)*3) )
-	{
-	    myReadfull( sockfd, (void *)resultfft, sizeof(float)*3 );
-	    printf("%e\t%e\t%e\n", resultfft[0], resultfft[1], resultfft[2]);
-	    i++;
-	}
+    while(1)
+    {
+	int32_t returnValue[2] = {0, 0};
+        float result = 0.0;
+	myWrite( sockfd, "MEAS\n" );
+        myReadfull( sockfd, (void *)returnValue, sizeof(int32_t)*2 );
+	assert( returnValue[1] == sizeof(float));
+        myReadfull( sockfd, (void *)&result, sizeof(float));
+	printf("%e\n", result);	
+	sleep(1);
+    }
     close( sockfd );
     return EXIT_SUCCESS;
 }
