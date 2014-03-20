7e291d5c2595718169be9c7f8c4dbd6074f0d0aa
Mathias Helsen <mathias.helsen@ugent.be>
2014-03-20 15:18:25 +0100

    dummy

--- Example/main.c
+++ Example/main.c
@@ -6,22 +6,31 @@
 #include <assert.h>
 #include <unistd.h>
 
+#define _BSD_SOURCE
+
 int main(int argc, char **argv)
 {
     int sockfd = 0;
 
-    initClient( &sockfd, 5002);
+    initClient( &sockfd, 5004);
 
-    while(1){
+    //while(1){
 	int32_t returnValue[2] = {0, 0};
-        float result[3];
+        float result[8];
 	myWrite( sockfd, "MEAS,1.0,18.0e3\n" );
         myReadfull( sockfd, (void *)returnValue, sizeof(int32_t)*2 );
-	assert( returnValue[1] == sizeof(float));
-        myReadfull( sockfd, (void *)result, sizeof(float));
-	printf("%e\n", result[0]);	
-	sleep(1);
-    }
+	assert( returnValue[1] == 3*sizeof(float));
+        myReadfull( sockfd, (void *)result, 3*sizeof(float));
+	printf("%f mA\n" , result[0]);
+	/*
+	for(int i = 0; i < 8; i++ )
+	{
+	    printf("%f\t", result[i]);
+	}
+	printf("\n");
+	*/
+	usleep(250*1000);
+    //}
     close( sockfd );
     return EXIT_SUCCESS;
 }
