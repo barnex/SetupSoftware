f537c6cffa7ad40c36eaf37bba8fc1ab825c01fc
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2015-05-28 11:45:34 +0200

    FFTGrabber: fallback to dynamic linking of needed

--- FFTGrabber/make.sh
+++ FFTGrabber/make.sh
@@ -3,5 +3,12 @@ set -e
 set -v
 
 gcc -c -std=c99 -Wall -Werror -O0 -g *.c
-libs="/usr/lib64/librfftw.a /usr/lib64/libfftw.a /usr/lib/libportaudio.a -lasound"
-gcc -lm *.o ../Libraries/*.o $libs -o FFTGrabber
+
+#libs for static linking
+static="/usr/lib64/librfftw.a /usr/lib64/libfftw.a /usr/lib/libportaudio.a -lasound"
+
+#libs for dynamic linking
+dynamic="-lrfftw -lfftw -lportaudio"
+
+# try static linking first, fallback to dynamic
+gcc -lm *.o ../Libraries/*.o $static -o FFTGrabber 2> /dev/null || gcc *.o ../Libraries/*.o $dynamic -lpthread -lm -lrt -ldl -o FFTGrabber
